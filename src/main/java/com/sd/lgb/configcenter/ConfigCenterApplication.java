40e16932a64b993776a2314c51de97cfda72842f
liugb <liugb@riverroad.cn>
2019-10-11 15:26:02 +0800

    init

---
+++ src/main/java/com/sd/lgb/configcenter/ConfigCenterApplication.java
@@ -0,0 +1,19 @@
+package com.sd.lgb.configcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author liugb
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApplication.class, args);
+    }
+
+}
+
